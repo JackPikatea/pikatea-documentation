ed473ba545687d8de1e15607276e5b201912febc
Jack Kester <66742583+JackPikatea@users.noreply.github.com>
2022-05-20 13:13:36 -0500

    add youtube video

--- docs/.vuepress/components/Uses.vue
+++ docs/.vuepress/components/Uses.vue
@@ -28,7 +28,7 @@
                 <span class="sr-only">(opens new window)</span>
               </span>
           </a>
-          
+          <center><iframe width="560" height="315" src="https://www.youtube.com/embed/wpA1vrWgPRY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
           </p>
       </li>
       <li>
